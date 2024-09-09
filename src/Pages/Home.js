6c2afd61352dcb0e5e865c09347df680de1a56d8
8sosa <emmanuel.amos@stu.cu.edu.ng>
2024-09-09 11:17:29 +0100

    homepage commit
    Gigi made some changes to the homepage design post development but we love her so we did not charge.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Home.css'
 import InfiniteCarousel from '../Components/InfiniteCarousel'
-import {Container} from 'react-bootstrap'
-import Slider from '../Components/Slider'
+import {Col, Container, Row} from 'react-bootstrap'
+// import Slider from '../Components/Slider'
+import Pic1 from '../Images/IMG_4612.JPG';
+import Pic2 from '../Images/IMG_4613.JPG';
+import Pic3 from '../Images/IMG_4614.JPG';
 import Reach from '../Components/Reach'
 
 export default function Home() {
@@ -12,7 +15,18 @@ export default function Home() {
       <Container className='carouselContainer'>
         <InfiniteCarousel />
       </Container>
-        <section className='currentWork'>
+      <Row className='homeImgRow'>
+        <Col className='homeImgCol'><img src={Pic1} alt='' className='homeImg'/></Col>
+        <Col className='homeImgCol'><img src={Pic2} alt='' className='homeImg'/></Col>
+        <Col className='homeImgCol'><img src={Pic3} alt='' className='homeImg'/></Col>
+      </Row>
+      <p className='homeQuote mont'>"There will be a quote here, that'll embody the aura of the artworks which i'll send to you."</p>
+      <Row className='homeImgRow'>
+        <Col className='homeImgCol'><img src={Pic1} alt='' className='homeImg'/></Col>
+        <Col className='homeImgCol'><img src={Pic2} alt='' className='homeImg'/></Col>
+        <Col className='homeImgCol'><img src={Pic3} alt='' className='homeImg'/></Col>
+      </Row>
+        {/* <section className='currentWork'>
           <div className='overlay'></div>
           <div className='content' >
             <h1 className='contentHeader altMont'>EBO OKAN</h1>
@@ -21,7 +35,7 @@ export default function Home() {
             <Slider />
             <a href="/project"><button className='moreBtn mont'>View More</button></a>
           </div>
-        </section>
+        </section> */}
       <Container>
         <Reach />
       </Container>
